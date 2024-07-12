03215eb0a6987063adb1561a26ed4b8e75dd5291
Gaurav Jadhav <gauravjadhav173@gmail.com>
2024-07-12 16:33:01 +0530

    file changes for deploymant

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const dashboardController = require('./controller/controller');
+const dashboardController = require('./Controller/controller');
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
